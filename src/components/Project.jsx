f6d82e0f9b83de484bacdfdc054af023e9d2a0e8
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2024-12-02 00:35:53 -0600

    Portfolio and Project components

---
+++ src/components/Project.jsx
@@ -0,0 +1,49 @@
+function Project() {
+    return (
+        <div className="projects">
+            <section className="card">
+                <header>Awesome Weather</header>
+                <img src="./assets/images/camera.jpg" alt="Project Image" />
+                <p>Application URL    : https://awesome-weather.onrender.com</p>
+                <p>GitHub URL         : https://github.com/Mahesh-GH24/awesome-weather</p>
+             </section>
+            
+            <section className="card">
+                <header>Wax-on-Wax-off</header>
+                <img src="./assets/images/camera.jpg" alt="Project Image" />
+                <p>Application URL    : https://terrencethe1.github.io/wax-on-wax-off</p>
+                <p>GitHub URL         : https://github.com/terrencethe1/wax-on-wax-off</p>
+            </section>
+       
+            <section className="card">
+                <header>Effortless Readme Generator</header>
+                <img src="./assets/images/camera.jpg" alt="Project Image" />
+                <p>Application URL    : None</p>
+                <p>GitHub URL         : https://github.com/Mahesh-GH24/effortless-readme-generato</p>
+            </section>
+       
+            <section className="card">
+                <header>Modernistic Portfolio</header>
+                <img src="./assets/images/camera.jpg" alt="Project Image" />
+                <p>Application URL    : None</p>
+                <p>GitHub URL         : https://github.com/Mahesh-GH24/modernistic-portfolio</p>
+            </section>
+        
+            <section className="card">
+                <header>Awesome Vehicle Builder</header>
+                <img src="./assets/images/camera.jpg" alt="Project Image" />
+                <p>Application URL    :None</p>
+                <p>GitHub URL         : https://github.com/Mahesh-GH24/Vehicle_Builder</p>
+            </section>
+       
+            <section className="card">
+                <header>Terrific Employee Tracker System</header>
+                <img src="./assets/images/camera.jpg" alt="Project Image" />
+                <p>Application URL    : None</p>
+                <p>GitHub URL         : https://github.com/Mahesh-GH24/terrific-cms</p>
+            </section>
+        </div>               
+    );
+}
+
+export default Project;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,14 @@
+import Project from '../components/Project'
+
 export default function Portfolio() {
     return (
       <div>
-        <h1>Portfolio Page</h1>
+        <h3>Portfolio Page</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
+         This displays all the wonderful projects that I have built over time!
         </p>
+        <Project />
       </div>
     );
   }
-  
+
